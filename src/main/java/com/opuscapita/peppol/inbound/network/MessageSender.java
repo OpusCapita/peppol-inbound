db4eb151426d95da1ea78c2b96c35c26957694ee
thebilge <ibrahimbilge7@gmail.com>
2019-03-28 09:18:23 +0100

    trying a different base image

--- src/main/java/com/opuscapita/peppol/inbound/network/MessageSender.java
+++ src/main/java/com/opuscapita/peppol/inbound/network/MessageSender.java
@@ -1,9 +1,9 @@
 package com.opuscapita.peppol.inbound.network;
 
 import com.opuscapita.peppol.commons.container.ContainerMessage;
+import com.opuscapita.peppol.commons.eventing.EventReporter;
 import com.opuscapita.peppol.commons.eventing.TicketReporter;
 import com.opuscapita.peppol.commons.queue.MessageQueue;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,36 +18,28 @@ public class MessageSender {
     @Value("${peppol.inbound.queue.out.name}")
     private String outputQueue;
 
-    @Value("${peppol.eventing.queue.in.name}")
-    private String eventingQueue;
-
     private final MessageQueue messageQueue;
+    private final EventReporter eventReporter;
     private final TicketReporter ticketReporter;
 
     @Autowired
-    public MessageSender(@NotNull MessageQueue messageQueue, @NotNull TicketReporter ticketReporter) {
+    public MessageSender(MessageQueue messageQueue, EventReporter eventReporter, TicketReporter ticketReporter) {
         this.messageQueue = messageQueue;
+        this.eventReporter = eventReporter;
         this.ticketReporter = ticketReporter;
     }
 
     // no exception must be thrown by this method
     // in case of a failure we have local file to reprocess
     void send(ContainerMessage cm) {
+        eventReporter.reportStatus(cm);
+
         try {
             messageQueue.convertAndSend(outputQueue, cm);
             logger.info("Message sent to " + outputQueue + " queue, about file " + cm.toKibana());
         } catch (Exception e) {
             logger.error("Failed to report received file " + cm.getFileName() + " to queue " + outputQueue, e);
             ticketReporter.reportWithContainerMessage(cm, e, "Failed to report received file " + cm.getFileName() + " to queue " + outputQueue);
-            return;
-        }
-
-        try {
-            messageQueue.convertAndSend(eventingQueue, cm);
-            logger.info("Message sent to " + eventingQueue + " queue, about file " + cm.toKibana());
-        } catch (Exception e) {
-            logger.error("Failed to report received file " + cm.getFileName() + " status to " + eventingQueue + " queue");
-            ticketReporter.reportWithContainerMessage(cm, e, "Failed to report file " + cm.getFileName() + " status to queue " + outputQueue);
         }
     }
 
