3b249c65918d9d7bc3efcc668ade7ddf4b2435e2
thebilge <ibrahimbilge7@gmail.com>
2019-09-10 10:06:37 +0200

    adding oxalis-as4 support

--- src/main/java/no/difi/oxalis/as2/inbound/GuiceBeansConfig.java
+++ src/main/java/no/difi/oxalis/as2/inbound/GuiceBeansConfig.java
@@ -6,6 +6,7 @@ import com.opuscapita.peppol.inbound.rest.InboundBusinessServlet;
 import com.opuscapita.peppol.inbound.rest.InboundHomeServlet;
 import com.opuscapita.peppol.inbound.rest.InboundStatusServlet;
 import com.opuscapita.peppol.inbound.rest.StatisticsServlet;
+import no.difi.oxalis.as4.inbound.As4Servlet;
 import no.difi.oxalis.commons.guice.GuiceModuleLoader;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -61,6 +62,14 @@ public class GuiceBeansConfig {
     public ServletRegistrationBean<HttpServlet> as2ServletBean() {
         ServletRegistrationBean<HttpServlet> bean = new ServletRegistrationBean<>(
                 injector.getInstance(As2Servlet.class), "/public/as2");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<HttpServlet> as4ServletBean() {
+        ServletRegistrationBean<HttpServlet> bean = new ServletRegistrationBean<>(
+                injector.getInstance(As4Servlet.class), "/public/as4");
         bean.setLoadOnStartup(1);
         return bean;
     }
