27fb888264ffe1efbe2d7cb70596b8228c11968c
thebilge <ibrahimbilge7@gmail.com>
2019-11-13 15:45:18 +0100

    removing logs

--- src/main/java/com/opuscapita/peppol/inbound/network/MessageHandler.java
+++ src/main/java/com/opuscapita/peppol/inbound/network/MessageHandler.java
@@ -52,24 +52,15 @@ public class MessageHandler {
 
     // this is the only method that allowed to throw an exception which will be propagated to the sending party
     String store(String filename, Source source, InputStream inputStream) throws IOException {
-
-        String result = null;
-
         try {
             logger.debug("MesssageHandler.store invoked for filename: " + filename);
             String path = hotFolder + StorageUtils.FILE_SEPARATOR + source.name().toLowerCase();
             path = StorageUtils.createDailyPath(path, "");
-            result = storage.put(inputStream, path, filename);
+            return storage.put(inputStream, path, filename);
         } catch (Exception e) {
             fail("Failed to store message " + filename, filename, e);
             throw new IOException("Failed to store message " + filename + ", reason: " + e.getMessage(), e);
         }
-
-        try (InputStream content = storage.get(result)) {
-            logger.info(IOUtils.toString(content));
-        }
-
-        return result;
     }
 
     ContainerMessageMetadata extractMetadata(ServletRequestWrapper wrapper) throws IOException {
