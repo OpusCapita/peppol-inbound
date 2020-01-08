8d3b0b677c390ccb3786f825e3502b71b2a007d2
thebilge <ibrahimbilge7@gmail.com>
2020-01-08 17:09:33 +0100

    implementing as4 api endpoint v7

--- src/main/java/com/opuscapita/peppol/inbound/InboundModule.java
+++ src/main/java/com/opuscapita/peppol/inbound/InboundModule.java
@@ -1,9 +1,12 @@
 package com.opuscapita.peppol.inbound;
 
 import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.opuscapita.peppol.inbound.business.BusinessInboundPersister;
 import com.opuscapita.peppol.inbound.network.InboundHandler;
 import no.difi.oxalis.api.inbound.InboundService;
+import no.difi.oxalis.api.lang.OxalisLoadingException;
 import no.difi.oxalis.api.persist.PayloadPersister;
 import no.difi.oxalis.api.persist.PersisterHandler;
 import no.difi.oxalis.api.persist.ReceiptPersister;
@@ -13,6 +16,14 @@ import no.difi.oxalis.as4.config.TrustStore;
 import no.difi.oxalis.as4.inbound.*;
 import no.difi.oxalis.commons.settings.SettingsBuilder;
 import org.apache.cxf.wsdl.interceptors.AbstractEndpointSelectionInterceptor;
+
+import javax.inject.Named;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 public class InboundModule extends OxalisModule implements Module {
 
@@ -30,6 +41,19 @@ public class InboundModule extends OxalisModule implements Module {
         bind(As4EndpointsPublisher.class).to(As4EndpointsPublisherImpl.class);
         bind(As4InboundHandler.class);
         SettingsBuilder.with(this.binder(), TrustStore.class);
+    }
+
+    @Provides
+    @Singleton
+    @Named("truststore-ap")
+    protected KeyStore getTruststoreAp() {
+        try (InputStream inputStream = getClass().getResourceAsStream("peppol-truststore.jks")) {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(inputStream, "peppol".toCharArray());
+            return keyStore;
+        } catch (KeyStoreException | IOException | CertificateException | NoSuchAlgorithmException e) {
+            throw new OxalisLoadingException("Unable to load truststore for AP.", e);
+        }
     }
 
 }
