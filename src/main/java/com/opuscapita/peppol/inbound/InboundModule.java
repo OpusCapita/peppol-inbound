be8191bed893df5c8e1d4e6d11274b01fdb869bf
thebilge <ibrahimbilge7@gmail.com>
2020-01-09 11:12:24 +0100

    removing truststore after oxalis update

--- src/main/java/com/opuscapita/peppol/inbound/InboundModule.java
+++ src/main/java/com/opuscapita/peppol/inbound/InboundModule.java
@@ -1,29 +1,18 @@
 package com.opuscapita.peppol.inbound;
 
 import com.google.inject.Module;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import com.opuscapita.peppol.inbound.business.BusinessInboundPersister;
 import com.opuscapita.peppol.inbound.network.InboundHandler;
 import no.difi.oxalis.api.inbound.InboundService;
-import no.difi.oxalis.api.lang.OxalisLoadingException;
 import no.difi.oxalis.api.persist.PayloadPersister;
 import no.difi.oxalis.api.persist.PersisterHandler;
 import no.difi.oxalis.api.persist.ReceiptPersister;
 import no.difi.oxalis.as2.inbound.OcInboundService;
-import no.difi.oxalis.commons.guice.OxalisModule;
 import no.difi.oxalis.as4.config.TrustStore;
 import no.difi.oxalis.as4.inbound.*;
+import no.difi.oxalis.commons.guice.OxalisModule;
 import no.difi.oxalis.commons.settings.SettingsBuilder;
 import org.apache.cxf.wsdl.interceptors.AbstractEndpointSelectionInterceptor;
-
-import javax.inject.Named;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
 public class InboundModule extends OxalisModule implements Module {
 
@@ -42,18 +31,4 @@ public class InboundModule extends OxalisModule implements Module {
         bind(As4InboundHandler.class);
         SettingsBuilder.with(this.binder(), TrustStore.class);
     }
-
-    @Provides
-    @Singleton
-    @Named("truststore-ap")
-    protected KeyStore getTruststoreAp() {
-        try (InputStream inputStream = getClass().getResourceAsStream("peppol-truststore.jks")) {
-            KeyStore keyStore = KeyStore.getInstance("JKS");
-            keyStore.load(inputStream, "peppol".toCharArray());
-            return keyStore;
-        } catch (KeyStoreException | IOException | CertificateException | NoSuchAlgorithmException e) {
-            throw new OxalisLoadingException("Unable to load truststore for AP.", e);
-        }
-    }
-
 }
