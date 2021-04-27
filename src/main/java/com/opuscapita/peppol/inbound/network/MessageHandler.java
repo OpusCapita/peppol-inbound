dcc33ca4fdea910255e127cd24b36c1637d9ce73
Dusty Wilhelm Murray <wilhelm71@users.noreply.github.com>
2021-04-27 13:34:38 +0200

    Update MessageHandler.java

--- src/main/java/com/opuscapita/peppol/inbound/network/MessageHandler.java
+++ src/main/java/com/opuscapita/peppol/inbound/network/MessageHandler.java
@@ -60,11 +60,11 @@ public class MessageHandler {
             path = StorageUtils.createDailyPath(path, "");
 
             if( inputStream.available() == 0 ) {
-                logger.warn("Number of bytes on stream is zero");
+                logger.warn("Number of bytes on stream is zero for " + filename);
 
                 throw new Exception( "Number of bytes on stream is zero" );
             }
-            
+
             String result = storage.put(inputStream, path, filename);
 
             return  result;
